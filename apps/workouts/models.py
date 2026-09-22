bd7e0170d19ac6361b8eb94f1defdf5a73a5827d
Murilo <murilodasilvadaschagas12@gmail.com>
2026-09-21 23:03:04 -0300

    feat: criados modelos - workout, workout_splits, workout_type.

--- apps/workouts/models.py
+++ apps/workouts/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from os import name
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Workout_type(models.Model):
+
+    name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50)
+    color = models.CharField(max_length=7)
+
+class Workout(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workout_type = models.ForeignKey(Workout_type, on_delete=models.PROTECT)
+
+    date = models.DateField('Data')
+    distace_km = models.DecimalField('Distancia_km', max_digits=5, decimal_places=2)
+    duration_sec = models.IntegerField()
+    pace_sec_km = models.IntegerField()
+    speed_kmh = models.DecimalField('Velocidade_kmh', max_digits=4, decimal_places=2)
+    hr_avg = models.SmallIntegerField()
+    hr_max = models.SmallIntegerField()
+    cadence_spm = models.SmallIntegerField()
+    elavation_m = models.SmallIntegerField()
+    calories = models.SmallIntegerField()
+    rpe = models.SmallIntegerField()
+    notes = models.TextField('Observações', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Workout_splits(models.Model):
+
+    workout = models.ForeignKey(Workout, on_delete=models.PROTECT)
+    split_number = models.SmallIntegerField()
+    distace_km = models.DecimalField('Distancia_km', max_digits=5, decimal_places=2)
+    pace_sec_km = models.IntegerField()
+    pace_sec_km = models.IntegerField()
+    elavation_m = models.SmallIntegerField()
