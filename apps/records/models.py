5125f5effcfaacfb601af568e8f149dc96e1f9af
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-09-23 23:45:45 -0300

    feat: Models 'records'

--- apps/records/models.py
+++ apps/records/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from workouts.models import Workout
+
+class RecordTypes(models.Model):
+
+    name = models.CharField(max_length=50)
+    distance_km = models.DecimalField(max_digits=5, decimal_places=2)
+
+class PersonalRecords(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    record_types = models.ForeignKey(RecordTypes, on_delete=models.PROTECT)
+    workout = models.ForeignKey(Workout, on_delete=models.PROTECT)
+
+    value = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateTimeField()
