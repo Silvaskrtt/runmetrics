3f6f3e50f3e2a177b410801e689f22f8f853753c
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-09-23 23:48:52 -0300

    feat: Criando Tabelas do App metrics

--- apps/metrics/models.py
+++ apps/metrics/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class DailyMetrics(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    date = models.DateTimeField()
+    sleep_quality = models.SmallIntegerField()
+    energy = models.SmallIntegerField()
+    legs_feeling = models.SmallIntegerField()
+    breathing = models.SmallIntegerField()
+    pain_level = models.SmallIntegerField()
+    notes = models.TextField('Obersevações', blank=True, null=True)
